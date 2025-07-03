21853138c62741cdf1421bf0c89ff73504210978
DeviHafida <144782420+DeviHafida@users.noreply.github.com>
2025-07-03 20:31:08 +0800

    Delete controllers directory

--- controllers/admin/artikel/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../../config/database.php';
-require_once '../../../models/Artikel.php';
-
-$db = (new Database())->getConnection();
-$artikel = new Artikel($db);
-$data = $artikel->getAll();
-
-include '../../../views/admin/artikel/index.php';
